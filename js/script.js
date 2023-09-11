40a9247aec75d1f8aca8f4fac3f1864b81483fa3
Jacob <Azalea9x@gmail.com>
2023-09-11 01:14:55 -0400

    Update

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
  alert(window.innerWidth); 
 
  const mediaQuery = window.matchMedia('(min-width: 1800px) and (orientation:landscape');
- if (mediaQuery.matches) {
+
+ const breadcrumb = document.querySelector(".ProductItem-nav-breadcrumb-link, .ProductItem-nav-breadcrumb-separator");
+ 
+ 
+ if ((mediaQuery.matches) && (breadcrumb != null))
+ {
+    breadcrumb.style.transform = "translateY(-30%) !important"; 
    alert('landscape');
  }
