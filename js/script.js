cbaebdeff36d5c44c62cc5e61082c08dfce538d3
Jacob <Azalea9x@gmail.com>
2023-09-11 01:21:05 -0400

    Update

--- js/script.js
+++ js/script.js
@@ -2,11 +2,14 @@
 
  const mediaQuery = window.matchMedia('(min-width: 1800px) and (orientation:landscape');
 
- const breadcrumb = document.querySelector(".ProductItem-nav-breadcrumb-link, .ProductItem-nav-breadcrumb-separator");
  
  
- if ((mediaQuery.matches) && (breadcrumb != null))
+ if ((mediaQuery.matches) && (document.querySelector(".ProductItem-nav-breadcrumb-link") && (document.querySelector(".ProductItem-nav-breadcrumb-separator"))) != null)
  {
-    breadcrumb.style.transform = "translateY(-30%) !important"; 
+    document.querySelector(".ProductItem-nav-breadcrumb-link").style.transform = "translateY(-30%) !important"; 
+
+    document.querySelector(".ProductItem-nav-breadcrumb-separator").style.transform = "translateY(-30%) !important";
    alert('landscape');
  }
+
+ //https://css-tricks.com/working-with-javascript-media-queries/
